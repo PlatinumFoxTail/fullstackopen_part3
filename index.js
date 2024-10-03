23c300f88eace53676b63fa97b6bb22e5fc7a573
Martin Häggblom <martin.haggblom@gmail.com>
2024-10-03 08:47:46 +0300

    submit 3.2: Phonebook backend step 2

--- index.js
+++ index.js
@@ -24,15 +24,21 @@ let persons = [
       }
   ]
 
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
+app.get('/info', (request, response) => {
+    const totalPersons = persons.length;
+    const todayDate = new Date();
+    
+    response.send(`
+        <p>Phonebook has info for ${totalPersons} people</p>
+        <p>${todayDate}</p>
+    `);
+})
   
-  app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response) => {
     response.json(persons)
-  })
+})
   
-  const PORT = 3001
-  app.listen(PORT, () => {
+const PORT = 3001
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-  })
+})
