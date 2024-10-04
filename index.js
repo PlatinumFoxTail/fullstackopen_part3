915a2cf51e604bb9e80bf21087930ea7449a8ae9
Martin Häggblom <martin.haggblom@gmail.com>
2024-10-04 11:24:01 +0300

    backend-frontend connection attempt 3.14: Phonebook database, step 2

--- index.js
+++ index.js
@@ -13,29 +13,6 @@ morgan.token('body', (req) => {
 //use morgan middleware based on tiny format and custom logging for POST requests
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-        "id": "1",
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-      },
-      { 
-        "id": "2",
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-      },
-      { 
-        "id": "3",
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-      },
-      { 
-        "id": "4",
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-      }
-  ]
-
 app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
@@ -68,18 +45,10 @@ app.get('/info', (request, response) => {
 })
   
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
-
-//app.get('/api/persons/:id', (request, response) => {
-    //const id = request.params.id
-    //const person = persons.find(person => person.id === id)
-    //if (person) {
-        //response.json(person)
-    //} else {
-        //response.status(404).end()
-    //}
-//})
 
 app.get('/api/persons/:id', (request, response) => {
     Person.findById(request.params.id).then(person => {
@@ -97,47 +66,18 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
     const body = request.body;
 
-    //check name and number provided
-    //if (!person.name || !person.number) {
-        //return response.status(400).json({
-            //error: 'The name or number is missing'
-        //});
-    //}
-
-    if (body.content === undefined) {
-        return response.status(400).json({ error: 'content missing' })
-    }
-
-    //check if name already occupied
-    const nameOccupied = persons.some(p => p.name === person.name);
-    if (nameOccupied) {
-        return response.status(400).json({ 
-            error: 'The name already exists in the phonebook' 
-        });
-    }
-
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max)
-    }
-    const randomId = getRandomInt(10000)
-
-    //const newPerson = {
-        //id: String(randomId),
-        //name: person.name,
-        //number: person.number
-    //};
+    if (!body.name || !body.number) {
+        return response.status(400).json({ error: 'The name or number is missing' })
+      }
 
     const person = new Person({
         name: body.name,
         number: body.number,
-    })
-    
-    //persons = persons.concat(newPerson)
-    //response.json(newPerson)
-
+    });
+      
     person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+        response.json(savedPerson);
+    });
 });
 
 app.use(unknownEndpoint)
