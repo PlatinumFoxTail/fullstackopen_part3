017bddae7a80728e64508ffbaaeae474b7e67e78
Martin Häggblom <martin.haggblom@gmail.com>
2024-10-04 12:01:26 +0300

    backend-database integration working for saving new persons

--- index.js
+++ index.js
@@ -13,6 +13,29 @@ morgan.token('body', (req) => {
 //use morgan middleware based on tiny format and custom logging for POST requests
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+let persons = [
+    { 
+        "id": "1",
+        "name": "Arto Hellas", 
+        "number": "040-123456"
+      },
+      { 
+        "id": "2",
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+      },
+      { 
+        "id": "3",
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+      },
+      { 
+        "id": "4",
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+      }
+  ]
+
 app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
@@ -45,16 +68,18 @@ app.get('/info', (request, response) => {
 })
   
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+    response.json(persons)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-      response.json(person)
-    })
-  })
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
@@ -66,15 +91,20 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
     const body = request.body;
 
+    //check if name and number are provided
     if (!body.name || !body.number) {
-        return response.status(400).json({ error: 'The name or number is missing' })
-      }
+        return response.status(400).json({ 
+            error: 'The name or number is missing' 
+        });
+    }
 
+    //create a new Person instance
     const person = new Person({
         name: body.name,
         number: body.number,
     });
-      
+
+    //save to MongoDB and respond with JSON
     person.save().then(savedPerson => {
         response.json(savedPerson);
     });
